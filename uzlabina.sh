52e54d9d715b23700eba0f013dd11c6a71788430
Czechball <david@jenne.cz>
2020-12-08 13:10:41 +0100

    fixed syntax because nostalgia

--- uzlabina.sh
+++ uzlabina.sh
@@ -3,7 +3,6 @@
 OKFORMAT="[\e[32mOK\e[0m   ]"
 WARNINGFORMAT="[\e[38;5;196mWARNING\e[0m]"
 ERRORFORMAT="[\e[33mERROR\e[0m]"
-INFOFORMAT="[\e[38;5;44mINFO\e[0m ]"
 
 if [[ $1 != "" ]]
 then
@@ -31,24 +30,24 @@ then
 		curl https://wifi.uzlabina.cz/ --silent > /dev/null
 	then
 		status=$(curl -X POST -F username=$username -F password=$password https://wifi.uzlabina.cz --silent --connect-timeout $timeout)
-		if echo $status | grep "Neplatné jméno nebo heslo" > /dev/null
+		if echo "$status" | grep "Neplatné jméno nebo heslo" > /dev/null
 			then
-				printf "$ERRORFORMAT Invalid username/password\n"
-			elif echo $status | grep "IP adresa je již povolena" > /dev/null
+				echo -e "$ERRORFORMAT Invalid username/password"
+			elif echo "$status" | grep "IP adresa je již povolena" > /dev/null
 				then
-					printf "$OKFORMAT You were already logged in\n"
+					echo -e "$OKFORMAT You were already logged in"
 			else
-				printf "$OKFORMAT \e[32mYou were successfully logged in\e[0m\n"
+				echo -e "$OKFORMAT \e[32mYou were successfully logged in\e[0m"
 			fi
 	else
-		printf "$ERRORFORMAT Úžlabina login is not available\nPlease try again later\n"
+		echo -e "$ERRORFORMAT Úžlabina login is not available\nPlease try again later"
 	fi
 else
 	if nmcli | grep "uzlabina" > /dev/null
 	then
-		printf "$WARNINGFORMAT \e[31mYou are not connected to a legitimate Úžlabina network!\nBe careful, your data may be compromised\e[0m\n"
+		echo -e "$WARNINGFORMAT \e[31mYou are not connected to a legitimate Úžlabina network!\nBe careful, your data may be compromised\e[0m"
 	else
-		printf "$ERRORFORMAT You are not connected to the Úžlabina WiFi network.\n"
+		echo -e "$ERRORFORMAT You are not connected to the Úžlabina WiFi network."
 	fi
 fi
 
